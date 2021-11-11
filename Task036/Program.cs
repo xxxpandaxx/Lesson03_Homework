6d91c286b992c9a3ed2475f25030520fee55e96e
xxxpandaxx <aman77345@gmail.com>
2021-11-11 07:22:32 +0300

    Homework

---
+++ Task036/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] arr = new int[10];
+
+Random rnd = new Random();
+int evenNumbers = 0;
+int oddNumbers = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(100, 999);
+    Console.Write(arr[i] + " ");
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0 )
+    {
+        evenNumbers++;
+    }
+    else
+    {
+        oddNumbers++;
+    }
+}
+
+Console.WriteLine($"\nколичество четных чисел : {evenNumbers}");
+Console.WriteLine($"количество не четных чисел : {oddNumbers}");
+
+
+
+
+
+
+
+
+
+
+
+
+
